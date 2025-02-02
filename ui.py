839fb576ff7ef2317dc91088448bf356cc265170
TuanBeliau <pianokotak@gmail.com>
2025-02-02 18:29:00 +0700

    update

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import paramiko
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 if "page" not in st.session_state:
     st.session_state.page = "login"
@@ -14,7 +15,7 @@ def login():
 
     host = st.text_input("Masukkan IP Addess", "192.168.43.5")
     username = st.text_input("Masukkan Username Mikrotik", value="admin")
-    password = st.text_input("Masukkan Password", type="password")
+    password = st.text_input("Masukkan Password", value=1, type="password")
     port = st.number_input("Port", 22, step=1)
     
     def connect():
@@ -38,16 +39,6 @@ def login():
     st.button("Hubungkan", on_click=connect)
 
 def home():
-    st.title("Halaman Konfigurasi")
-    st.write("Selamat Datang di Halaman Konfigurasi Mikrotik")
-
-    if st.session_state.koneksi:
-        st.write("Koneksi SSH Berhasil dipertahankan")
-
-        stdin, stdout, stderr = st.session_state.koneksi.exec_command("system resource print")
-        st.text(stdout.read().decode())
-    else :
-        st.error("Koneksi SSH tidak aktif")
 
     def logout() :
         if st.session_state.koneksi:
@@ -55,7 +46,115 @@ def home():
         st.session_state.koneksi = None
         st.session_state.page = "login"
 
-    st.button("Logout", on_click=logout)
+    st.title("Selamat Datang di Halaman Konfigurasi Mikrotik")
+
+    if st.session_state.koneksi:
+
+        with st.sidebar:
+            selected = option_menu("Menu", ["General", "Others", "Logout"],
+                                   icons=["bar-chart", "...", "box-arrow-left"],
+                                   menu_icon="house", default_index=0)
+            
+        # BAGIAN MENU GENERAL
+        if selected == "General":
+            st.write("Silahkan Lakukan Konfigurasi Mandiri")
+
+            def IP():
+                stdin, stdout, stderr = st.session_state.koneksi.exec_command("ip address print detail")
+                IPAddress_output = stdout.read().decode()
+
+                ip_data = []
+                for line in IPAddress_output.splitlines():
+                    if "address=" in line:
+                        interface = line.split("interface=")[1].split()[0]
+                        mentah = line.split("address=")[1].split()[0]
+                        address = mentah.split("/")[0]
+                        ip_data.append((interface, address))
+
+                if ip_data:
+                    new_ips = {}
+
+                    for interface, address in ip_data:
+                        new_ips[interface] = st.text_input(f"IP Address untuk {interface} :", value=address, key=f"ip_{interface}")
+                        
+                    return new_ips
+                else:
+                    st.error("Tidak koneksi yang aktif, Silakan login terlebih dahulu.")
+                    return None
+
+            def user():
+                try:
+                    new_password = st.text_input("Password Baru:", type="password", key="new_password")
+
+                    if new_password:
+                        return new_password
+                    else:
+                        return None
+                except Exception as e:
+                    st.error(f"Terjadi kesalahan saat memuat data pengguna: {e}")
+                    return None
+
+            # Mengambil data baru
+            new_ips = IP()
+            new_password = user()
+
+            if st.button("Simpan Perubahan"):
+                if new_ips or new_password:
+                    berhasil = True
+
+                    try:
+                        # Perubahan IP
+                        if new_ips:
+                            try:
+                                for interface, new_ip in new_ips.items():
+                                    ip_address = f"ip address set [find interface={interface}] address={new_ip}"
+                                    stdin, stdout, stderr = st.session_state.koneksi.exec_command(ip_address)
+                            except Exception as e:
+                                st.error(f"Gagal memperbarui IP Address: {e}")
+                                berhasil = False
+                        if new_password:
+                            try:
+                                ganti_pw = f"user set 0 password={new_password}"
+                                stdin, stdout, stderr = st.session_state.koneksi.exec_command(ganti_pw)
+                            except Exception as e:
+                                st.error(f"Error saat mengganti Password: {e}")
+                                berhasil = False
+
+                        if new_ips and new_password :
+                            try:
+                                for interface, new_ip in new_ips.items():
+                                    ip_address = f"ip address set [find interface={interface}] address={new_ip}"
+                                    stdin, stdout, stderr = st.session_state.koneksi.exec_command(ip_address)
+                                ganti_pw = f"user set 0 password={new_password}"
+                                stdin, stdout, stderr = st.session_state.koneksi.exec_command(ganti_pw)
+                            except Exception as e:
+                                st.error(f"Error saat memperbarui IP dan Password: {e}")
+                                berhasil = False
+                        else:
+                            st.error("Di Isi Ya bg")
+                    except Exception as e:
+                        st.error(f"Gagal memperbarui: {e}")
+                        berhasil = False
+
+
+                    if berhasil:
+                        @st.dialog("Perubahan Berhasil")
+                        def alert():
+                            st.success("Selamat! Konfigurasi Sudah Berhasil Di terapkan")
+                        alert()
+                        
+                else:
+                    st.error("Tolong isi salah satu field")
+
+        # -------------------------------------------------------------------------------------------------
+        
+        # BAGIAN MENU LOGOUT
+        if selected == "Logout" :
+            logout()
+            
+
+    else :
+        st.error("Koneksi SSH tidak aktif")
 
 if st.session_state.page == "login":
     login()
