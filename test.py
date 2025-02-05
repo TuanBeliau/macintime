9d6c371bbeda12045a8535cebe6b47432f4dadc4
TuanBeliau <pianokotak@gmail.com>
2025-02-05 11:53:33 +0700

    flask

--- test.py
+++ test.py
@@ -1,19 +1,45 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
+from flask import Flask, render_template, request
+import paramiko
 
-# Sidebar dengan option menu
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",  # Judul menu
-        options=["Home", "About"],  # Opsi menu
-        icons=["house", "info-circle"],  # Ikon untuk setiap menu
-        menu_icon="cast",  # Ikon menu utama
-        default_index=0  # Opsi default
-    )
+app = Flask(__name__)
 
-    # Tambahkan tombol di bawah option menu
-    if st.button("Klik Saya"):
-        st.sidebar.success("Tombol ditekan!")
+# Membuat define untuk melakukan koneksi SSH
+def koneksi_ssh(host, username, password, port):
+    """Aplikasi MaCInTime Untuk Melakukan Konfigurasi Mikrotik"""
+    try:
+        # Mencoba menghubungkan koneksi
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host, username=username, password=password, port=port)
 
-# Konten berdasarkan menu yang dipilih
-st.write(f"Anda memilih: {selected}")
+        # Menjalankan perintah
+        stdin, stdout, stderr = ssh.exec_command("ip address print detail")
+        output = stdout.read().decode()
+        ssh.close()
+
+        # Parsing data IP , split untuk memotong bagian, misal hasil command address=1 ..=.. ..=.. maka akan mengambil nilai indeks 1 (nilai address) dan menghapus sisanya dari indeks 0 split()[0]
+        data_ip = []
+        for item in output.splitlines():
+            if "interface=" in item and "address=" in item:
+                interface = item.split("interface=")[1].split()[0]
+                address = item.split("address=")[1].split()[0]
+                data_ip.append({"interface": interface, "address": address})
+
+        return data_ip
+    except Exception as e:
+        return str(e)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    hasil = None
+    if request.method == "POST":
+        host = request.form["host"]
+        username = request.form["username"]
+        password = request.form["password"]
+        port = int(request.form["port"])
+        hasil = koneksi_ssh(host, username, password, port)
+    
+    return render_template("index.html", hasil=hasil)
+
+if __name__ == "__main__":
+    app.run(debug=True)
