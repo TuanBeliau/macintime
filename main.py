b40f3fc5f3b7657a58083624a065f128513c0b51
TuanBeliau <pianokotak@gmail.com>
2025-03-10 10:04:41 +0700

    uodate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 import paramiko
 import uuid
+import time
 
 app = Flask(__name__)
 app.secret_key = "apalah"
@@ -37,6 +38,7 @@ def ssh_connect(host, username, password, port):
 
 @app.route("/", methods=["GET", "POST"])
 def login() :
+    berhasil = None
     error = session.pop("error", None)
 
     # Jika ada form di kirim dengan method POST 
@@ -49,14 +51,15 @@ def login() :
         # Memanggil def ssh_connect di awal dan mengirimkan variable di atas
         result = ssh_connect(host, username, password, port)
         if result is True:
-            return redirect(url_for("dashboard"))
+            berhasil = True
         else:
             return redirect(url_for("login"))
     
-    return render_template("login.html", error=error)
+    return render_template("login.html", error=error, berhasil=berhasil)
 
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
+    berhasil = None
     if not session.get("logged_in"):
         return redirect(url_for("login"))
 
@@ -66,7 +69,9 @@ def dashboard():
     if not ssh:
         return redirect(url_for("login"))
 
-    stdin, stdout, stderr = ssh.exec_command("interface print detail")
+    berhasil = True
+
+    stdin, stdout, stderr = ssh.exec_command("/interface print detail")
     output = stdout.read().decode()
 
     interface_all = []
@@ -77,7 +82,7 @@ def dashboard():
     
     def show_ip():
         try:
-            stdin, stdout, stderr = ssh.exec_command("ip address print detail")
+            stdin, stdout, stderr = ssh.exec_command("/ip address print detail")
             output = stdout.read().decode()
 
             ip_data = []
@@ -88,7 +93,7 @@ def dashboard():
                     flag = parts[1] # Mengambil nilai index ke 1 dari list parts alias statusnya, misal D
                     ip_id = parts[0] # Sama seperti di atas namun ini untuk id
                     
-                    address = next((part.split("address=")[1] for part in parts if "address=" in part), "Unknown") # Next membuat perulangan lebih singkat. Di sini akan mengambil nilai addres= (ini index 0)
+                    address = next((part.split("address=")[1] for part in parts if "address=" in part), "Unknown") # Next membuat perulangan lebih singkat. Di sini akan mengambil nilai address= (ini index 0)
                     interface = next((part.split("interface=")[1] for part in parts if "interface=" in part), "Unknown")
                     status = "dynamic" if "D" in flag else "static"
 
@@ -106,11 +111,11 @@ def dashboard():
 
     ip_list = show_ip()
 
-    return render_template("dashboard.html", host=session["host"], ip_list=ip_list, interfaces=interface_all)
+    return render_template("dashboard.html", host=session["host"], ip_list=ip_list, interfaces=interface_all, berhasil=berhasil)
 
 @app.route("/add_ip", methods=["POST"])
-def add_ip(address, interface):
-    if not session.get("logged_id"):
+def add_ip():
+    if not session.get("logged_in"):
         return redirect(url_for("login"))
     
     user_id = session.get("user_id")
@@ -119,17 +124,20 @@ def add_ip(address, interface):
     if not ssh:
         return redirect(url_for("login"))
 
-    if request.methods == "POST":
+    if request.method == "POST":
         address = request.form["address"]
+        prefix = int(request.form["prefix"])
         interface = request.form["interface"]
 
         try :
-            command = f"ip address add address={address} interface={interface}"
+            command = f"/ip address add address={address}/{prefix} interface={interface}"
             stdin, stdout, stderr = ssh.exec_command(command)
             stderr_output = stderr.read().decode()
              
             if stderr_output:
                 return f"Erorr when add IP: {stderr_output}"
+
+            return redirect(url_for("dashboard"))
         except Exception as e:
             return f"error occured : {e}"
 
@@ -147,7 +155,7 @@ def delete_ip(id):
             return redirect(url_for('login'))
         
         try:
-            command = f"ip address remove {id}"
+            command = f"/ip address remove {id}"
             stdin, stdout, stderr = ssh.exec_command(command)
             stderr_output = stderr.read().decode()
 
@@ -182,7 +190,7 @@ def change_ip(id):
 
         def change(address, prefix, interface, id):
             try:
-                command = f"ip address set {id} address={address}/{prefix} interface={interface}"
+                command = f"/ip address set {id} address={address}/{prefix} interface={interface}"
                 stdin, stdout, stderr = ssh.exec_command(command)
                 stderr_output = stderr.read().decode()
 
@@ -212,7 +220,7 @@ def settings() :
     if not ssh:
         return redirect(url_for("login"))
     
-    stdin, stdout, stderr = ssh.exec_command("system identity print")
+    stdin, stdout, stderr = ssh.exec_command("/system identity print")
     output = stdout.read().decode()
     current_identity = output.strip().split(": ")[1] if ": " in output else "Uknown"
     
@@ -222,7 +230,7 @@ def settings() :
 
         def ganti_identity(identity):
             try:
-                command = f"system identity set name={identity}"
+                command = f"/system identity set name={identity}"
                 stdin, stdout, stderr = ssh.exec_command(command)
                 stderr_output = stderr.read().decode()
 
@@ -262,7 +270,7 @@ def settings() :
 
     return render_template("setting.html", current_identity=current_identity)
 
-@app.route("/DHCP-Server")
+@app.route("/DHCP-Server", methods={"GET", "POST"})
 def dhcp():
     if not session.get("logged_in"):
         return redirect(url_for("login"))
@@ -270,15 +278,85 @@ def dhcp():
     user_id = session.get("user_id")
     ssh = ssh_connections.get(user_id)
 
-    if request.method == "POST":
-        ip_pool = request.form["ip_pool"]
+    if not ssh:
+        return redirect(url_for("login"))
 
+    try:
+        command_ip = "/ip address print detail"
+        stdin, stdout, stderr = ssh.exec_command(command_ip)
+        output = stdout.read().decode()
 
-        def dhcp_setup():
-            try:
-                ada
-            except Exception as e:
-                return f"Error: {e}"
+        addresses_all = []
+        for line in output.splitlines():
+            if "address" in line :
+                address = line.split("address=")[1].split()[0]
+                cek_oktet = address.split("/")[0].split(".")[3]
+                if cek_oktet == "1" or cek_oktet == "254" :
+                    addresses_all.append(address)
+
+    except Exception as e:
+        return render_template("DHCP.html")
+
+    if request.method == "POST": # Code Di bawah akan jalan jika terdeteksi method post
+        name = request.form["name"]
+        ip_address = request.form["ip_address"] # IP address dari select sebagai gateway
+        pool_range = int(request.form["pool_range"]) # Range IP
+
+        if not ip_address or pool_range <=0:
+            return "Input tidak valid"
+        
+        pool_name = f"dhcp_pool_{int(time.time())}" # Memberi nama unik untuk tiap pool_ip
+        gateway = ip_address.split("/")[0]
+        prefix = ip_address.split("/")[1]
+        base_ip = gateway.rsplit(".", 1)[0] # Memotong oktet ke-4
+        cek_ip = gateway.split(".")[3] # Ambil oktet ke-4
+
+        # Cek apakah oktet ke-4 .1/.254
+        if cek_ip == "1" :
+            pool_start = f"{base_ip}.2"
+            pool_end = f"{base_ip}.{pool_range + 1}"
+        else:
+            pool_start = f"{base_ip}.1"
+            pool_end = f"{base_ip}.{pool_range}"
+
+        pool_range = f"{pool_start}-{pool_end}"
+
+        try:
+            command_interface = f'/ip address print where address~"{gateway}"'
+            stdin, stdout, stderr = ssh.exec_command(command_interface)
+            output = stdout.read().decode()
+
+            interface = None
+            for line in output.splitlines():
+                hasil_output = line.split()
+                for kata in hasil_output:
+                    if kata.startswith("ether"):
+                        interface = kata
+                        break
+                if interface:
+                    break
+            
+            if not interface:
+                return "flash nyusul"
+
+        except Exception as e:
+            return f"my bad maybe {e}"
+
+        # Membuat command CLI
+        command_main = [
+            f"/ip pool add name={pool_name} ranges={pool_range}",
+            f"/ip dhcp-server network add address={base_ip}.0/{prefix} gateway={gateway} dns-server=8.8.8.8",
+            f"/ip dhcp-server add name={name} interface={interface} address-pool={pool_name} disabled=no"
+        ]
+
+        try:
+            for cmd in command_main:
+                ssh.exec_command(cmd)
+            return redirect(url_for("dashboard"))
+        except Exception as e:
+            return f"My bad maybe, dunno {e}"
+        
+    return render_template("DHCP.html", ip_address=addresses_all)
 
 @app.route("/logout")
 def logout():
